6cb1830925d0ee1a687b68ba5c4470d27c6e3177
藤田和希 <mentaianko@gmail.com>
2025-04-29 18:36:50 +0900

    投稿一覧機能の追加

--- home.php
+++ home.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+require_once 'db_connect.php';
 
+//投稿一覧取得
+$sql = "SELECT posts.*, users.username From posts INNER JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if(!isset($_SESSION['user_id'])){
   header('Location: login.php');
   exit;
@@ -11,11 +17,18 @@ if(!isset($_SESSION['user_id'])){
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
-    <title>ホーム</title>
+    <title>投稿一覧</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>こんにちは、<?= htmlspecialchars($_SESSION['username'], ENT_QUOTES); ?>さん！</h1>
+    <h1>投稿一覧</h1>
+    <?php foreach ($posts as $post): ?>
+      <div style="border:1px solid #ccc; padding:10px; margin:10px;">
+        <p><strong><?= htmlspecialchars($post['username']) ?></strong></p>
+        <p><?= htmlspecialchars($post['content'])?></p>
+        <p><em><?= $post['created_at']?></em></p>
+      </div>
+    <?php endforeach ?>
     <a href="logout.php">ログアウト</a>
     <a href="post.php">投稿画面へ進む</a>
   </body>
